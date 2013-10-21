b98d079a1c379aeab0572c0c239d2686cdcce4e8
Neil Leeder <nleeder@codeaurora.org>
2013-10-21 17:58:20 -0400

    msm: perf: clean up duplicate constraint events
    Events with a duplicate constraint are set to state=OFF when detected,
    so that their duplicate counts are not read. However, they were not being
    cleaned up because the core code only cleaned up ACTIVE events.
    This resulted in counters not being freed and eventually running out
    of resources.
    
    Clean up the events with state==OFF that were marked that way because of
    constraint duplication.
    Ensure counts are not updated for OFF events.
    
    Change-Id: If532801c79e6ad6809869eb0a3063774f00c92c3
    Signed-off-by: Neil Leeder <nleeder@codeaurora.org>
    
    Conflicts:
    	kernel/events/core.c

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -3026,6 +3026,15 @@ static void put_event(struct perf_event *event)
 
 	if (!atomic_long_dec_and_test(&event->refcount))
 		return;
+	/*
+	 * Event can be in state OFF because of a constraint check.
+	 * Change to ACTIVE so that it gets cleaned up correctly.
+	 */
+	if ((event->state == PERF_EVENT_STATE_OFF) &&
+	    event->attr.constraint_duplicate)
+		event->state = PERF_EVENT_STATE_ACTIVE;
+
+	file->private_data = NULL;
 
 	rcu_read_lock();
 	owner = ACCESS_ONCE(event->owner);
