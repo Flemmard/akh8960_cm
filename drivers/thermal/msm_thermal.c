eb5c709191134557d9cc8fb101abc4320888c9b8
Ivan Grinko <iivanich@gmail.com>
2014-08-03 15:54:00 +0300

    Revert "Sync cpufreq with CAF 3.5"
    This reverts commit fbd355aad498c713e4437f406c57b708e063e890.

--- drivers/thermal/msm_thermal.c
+++ drivers/thermal/msm_thermal.c
@@ -23,18 +23,7 @@
 #include <linux/msm_thermal.h>
 #include <linux/platform_device.h>
 #include <linux/of.h>
-#include <linux/err.h>
-#include <linux/slab.h>
-#include <linux/of.h>
-#include <linux/sysfs.h>
-#include <linux/types.h>
-#include <linux/android_alarm.h>
-#include <linux/thermal.h>
-#include <mach/rpm-regulator.h>
-#include <mach/rpm-regulator-smd.h>
-#include <linux/regulator/consumer.h>
-
-#define MAX_RAILS 5
+#include <mach/cpufreq.h>
 
 static int enabled;
 static struct msm_thermal_data msm_thermal_info;
